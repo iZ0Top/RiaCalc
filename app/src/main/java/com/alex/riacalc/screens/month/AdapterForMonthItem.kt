c1b4385dd074961776c8e95cb84b9d19ac986ffc
Alex <alex.sk990@gmail.com>
2024-12-15 23:10:10 +0200

    03

--- app/src/main/java/com/alex/riacalc/screens/month/AdapterForMonthItem.kt
+++ app/src/main/java/com/alex/riacalc/screens/month/AdapterForMonthItem.kt
@@ -3,8 +3,10 @@ package com.alex.riacalc.screens.month
 import android.content.Context
 import android.content.res.ColorStateList
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.alex.riacalc.R
 import com.alex.riacalc.databinding.ItemGridBinding
@@ -35,21 +37,22 @@ class AdapterForMonthItem(
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         val event = listItem[position]
 
+        with(holder.binding.gridLine){
+            visibility = if (event.description == "") View.GONE else View.VISIBLE
+        }
+
         when(event.type){
             TYPE_INSPECTION -> {
-                holder.binding.gridId.text = context.getString(R.string.template_plus, event.cost)
-                holder.binding.gridId.backgroundTintList = changeColor(R.color.green_100)
-                if (event.description != ""){
-                    holder.binding.gridId.
-                }
+                holder.binding.gridText.text = context.getString(R.string.template_plus, event.cost)
+                holder.binding.gridText.backgroundTintList = changeColor(R.color.green_100)
             }
             TYPE_INSPECTION_CAR_DEALERSHIP, TYPE_INSPECTION_CAR_PARK, TYPE_INSPECTION_CONST_PROGRESS, TYPE_INSPECTION_OTHER -> {
-                holder.binding.gridId.text = context.getString(R.string.template_plus, event.cost)
-                holder.binding.gridId.backgroundTintList = changeColor(R.color.blue_100)
+                holder.binding.gridText.text = context.getString(R.string.template_plus, event.cost)
+                holder.binding.gridText.backgroundTintList = changeColor(R.color.blue_100)
             }
             TYPE_TRIP, TYPE_OTHER -> {
-                holder.binding.gridId.text = context.getString(R.string.template_minus, event.cost)
-                holder.binding.gridId.backgroundTintList = changeColor(R.color.red_100)
+                holder.binding.gridText.text = context.getString(R.string.template_minus, event.cost)
+                holder.binding.gridText.backgroundTintList = changeColor(R.color.red_100)
             }
         }
     }
